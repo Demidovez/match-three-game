b0c89db17c784d0abfd23420f81bc670678cbd27
Nikolay Demidovez <nikolaydemidovez@gmail.com>
2024-08-15 20:06:37 +0300

    added layout

---
+++ Assets/Components/Row/Scripts/Row.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace RowSpace
+{
+    public class Row : MonoBehaviour
+    {
+    
+    }
+}
---
+++ Assets/Components/Tile/Scripts/Tile.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace TileSpace
+{
+    public class Tile : MonoBehaviour
+    {
+    
+    }  
+}
+
